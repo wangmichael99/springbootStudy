a88a1e14b19c8d2464c90473c06527058260b143
Michael Wang <wangxiaolinmichael@hotmail.com>
2019-09-24 20:19:35 -0400

    add modules

---
+++ src/main/java/com/wxl/springbootStudy/controller/TestController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.wxl.springbootStudy.controller;
+
+/**
+ * @author michaelwang
+ *
+ */
+public class TestController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/wxl/springbootStudy/service/TestService.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.wxl.springbootStudy.service;
+
+/**
+ * @author michaelwang
+ *
+ */
+public class TestService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
